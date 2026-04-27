72f30bc9e42f4ed77994a7759b07b8476b067ce0
Alban Oroz <aoroz@Mac.lan>
2026-04-27 10:48:22 -0400

    Make harvey yellow serif

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-black">
-      <h1 className="text-[20vw] font-black uppercase tracking-tighter text-white leading-none">
+      <h1 className="text-[20vw] font-black uppercase tracking-tighter text-yellow-400 leading-none font-serif">
         harvey
       </h1>
     </div>
